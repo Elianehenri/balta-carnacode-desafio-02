3323a696316b561cf6563e9642ea05b3ec4bd742
mitsugui <julio.kawai@gmail.com>
2024-02-12 22:33:25 -0300

    Adição de serviço para armazenar o resultado da medida

---
+++ IMC/Services/BmiService.cs
@@ -0,0 +1,43 @@
+﻿using IMC.Models;
+
+namespace IMC.Services
+{
+	public class BmiService
+    {
+        private readonly List<BMIModel> _models = new List<BMIModel>
+        {
+            new BMIModel
+            {
+                Altura = "186",
+                Peso = "86",
+                Mais65Anos = false,
+                Sexo = "Masculino"
+            },
+            new BMIModel
+            {
+                Altura = "160",
+                Peso = "55",
+                Mais65Anos = false,
+                Sexo = "Feminino"
+            },
+            new BMIModel
+            {
+                Altura = "170",
+                Peso = "80",
+                Mais65Anos = false,
+                Sexo = "Masculino"
+            }
+        };
+
+        public void Adicionar(BMIModel model)
+        {
+            _models.Add(model);
+        }
+
+		public List<BMIModel> Listar()
+        {
+            return _models;
+
+        }
+	}
+}
