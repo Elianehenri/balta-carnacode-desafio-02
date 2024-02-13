3276244e13c04136c9e9647c1eb795f48c27cc53
Gabriele Felice <gabriele_felice@hotmail.com>
2024-02-12 23:01:19 -0300

    Ajustes Model

--- IMC/Models/BMIModel.cs
+++ IMC/Models/BMIModel.cs
@@ -1,25 +1,7 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-
 namespace IMC.Models
 {
-    //    public class BMIFormModel
-    //    {
-    //        [Required(ErrorMessage = "A altura é obrigatória.")]
-    //        [Range(0.01,double.MaxValue, ErrorMessage = "A altura invalida.")]
-    //        public double Altura { get; set; }
-
-    //        [Required(ErrorMessage = "O peso é obrigatório.")]
-    //        [Range(1.0, double.MaxValue, ErrorMessage = "Peso invalido.")]
-    //        public double Peso { get; set; }
-
-    //        [Required(ErrorMessage = "O sexo é obrigatório.")]
-    //        public string Sexo { get; set; }
-
-
-    //        public bool Mais65Anos { get; set; }
-    //    }
-    //}
     public class BMIModel
     {
         [Required(ErrorMessage = "A altura é obrigatória.")]
@@ -35,9 +17,40 @@ namespace IMC.Models
 
         public bool Mais65Anos { get; set; }
 
-
         public DateTime DataCalculo { get; set; }
         public Double ValorCalculado { get; set; }
+        public BMISituationEnum Situacao { get; set; }
 
+        public BMISituationEnum GetSituacao()
+        {
+            if (ValorCalculado < 17.0)
+            {
+                return BMISituationEnum.MuitoAbaixoDoPeso;
+            }
+            else if (ValorCalculado >= 17.0 && ValorCalculado < 18.5)
+            {
+                return BMISituationEnum.AbaixoDoPeso;
+            }
+            else if (ValorCalculado >= 18.5 && ValorCalculado < 25)
+            {
+                return BMISituationEnum.PesoNormal;
+            }
+            else if (ValorCalculado >= 25 && ValorCalculado < 30)
+            {
+                return BMISituationEnum.AcimaDoPeso;
+            }
+            else if (ValorCalculado >= 30 && ValorCalculado < 35)
+            {
+                return BMISituationEnum.ObesidadeI;
+            }
+            else if (ValorCalculado >= 35 && ValorCalculado < 40)
+            {
+                return BMISituationEnum.ObesidadeII;
+            }
+            else
+            {
+                return BMISituationEnum.ObesidadeIII;
+            }
+        }
     }
 }
